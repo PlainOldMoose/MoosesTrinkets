c6e7a3f2ce2cbbae5fcdb1bc6aded2c6e6d354a0
Luque <luque@vandermerwe.uk>
2025-10-20 13:31:02 +0100

    Feat(command): Created skeleton command handler + basic test

--- src/main/java/me/plainoldmoose/command/TrinketCommandHandler.java
+++ src/main/java/me/plainoldmoose/command/TrinketCommandHandler.java
@@ -1,4 +1,54 @@
 package me.plainoldmoose.command;
 
-public class TrinketCommandHandler {
+import me.plainoldmoose.trinket.Trinket;
+import me.plainoldmoose.trinket.TrinketManager;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.xml.transform.OutputKeys;
+import java.util.List;
+
+public class TrinketCommandHandler implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args) {
+        if (!(commandSender instanceof Player)) {
+            return true;
+        }
+
+        Player player = (Player) commandSender;
+
+        if (args.length == 0) {
+            player.sendMessage(ChatColor.RED + "Usage: /trinket <give> <trinket>");
+        }
+
+        switch (args[0].toLowerCase()) {
+            case "give":
+                handleGive(player, args);
+                return true;
+            default:
+                player.sendMessage(ChatColor.RED + "Usage: /trinket <give> <trinket>");
+                return true;
+        }
+    }
+
+    private void handleGive(Player player, String[] args) {
+        if (args.length < 2) {
+            player.sendMessage(ChatColor.RED + "Usage: /trinkets give <trinket>");
+            return;
+        }
+
+        TrinketManager tm = TrinketManager.getInstance();
+        player.give(tm.get(args[1]).getItem());
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+        return List.of();
+    }
 }
--- src/test/java/Trinket/TrinketCommandTest.java
+++ src/test/java/Trinket/TrinketCommandTest.java
@@ -1,4 +1,44 @@
 package Trinket;
 
-public class TrinketCommandTest {
+import me.plainoldmoose.MoosesTrinkets;
+import me.plainoldmoose.command.TrinketCommandHandler;
+import org.bukkit.plugin.Plugin;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockbukkit.mockbukkit.MockBukkit;
+import org.mockbukkit.mockbukkit.ServerMock;
+import org.mockbukkit.mockbukkit.command.CommandMapMock;
+import org.mockbukkit.mockbukkit.entity.PlayerMock;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TrinketCommandTest {
+    TrinketCommandHandler tch = new TrinketCommandHandler();
+    private ServerMock server;
+    private PlayerMock player;
+    private Plugin plugin;
+
+
+    @BeforeEach
+    void setup() {
+        server = MockBukkit.mock();
+        plugin = MockBukkit.load(MoosesTrinkets.class);
+        player = server.addPlayer();
+    }
+
+    @AfterEach
+    void tearDown() {
+        MockBukkit.unmock();
+    }
+
+    @Test
+    void TrinketCommandTest() {
+        assertNotNull(server.getPluginCommand("trinket"));
+//        boolean result = tch.onCommand(player, server.getPluginCommand("trinket"), "trinkets", new String[]{}
+//        );
+//
+//        assertTrue(result);
+    }
 }
