78cafe51de0f5c2380268fc2d024655cdc06acb9
Luque <luque@vandermerwe.uk>
2025-10-31 16:15:12 +0000

    Feat(Trinket): Added default trinket for testing purposes

--- src/main/java/me/plainoldmoose/trinket/TrinketManager.java
+++ src/main/java/me/plainoldmoose/trinket/TrinketManager.java
@@ -1,18 +1,21 @@
 package me.plainoldmoose.trinket;
 
+import org.bukkit.Material;
+
 import java.util.HashMap;
 
 public class TrinketManager {
     private static TrinketManager instance = new TrinketManager();
+    private HashMap<String, Trinket> trinkets = new HashMap<>();
 
     private TrinketManager() {
+        trinkets.clear();
+        trinkets.put("hello", new Trinket("hello", Material.DIAMOND));
     }
 
     public static TrinketManager getInstance() {
         return instance;
     }
-
-    private HashMap<String, Trinket> trinkets = new HashMap<>();
 
     public void add(Trinket t) {
         trinkets.put(t.getDisplayName(), t);
