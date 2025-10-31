7c8123e1af644f77f762d9f676bc244f5cef14ba
Luque <luque@vandermerwe.uk>
2025-10-31 17:08:38 +0000

    Test(Manager): Implemented test to determine if an item stored in the manager has an appropriate NamespacedKey

--- src/test/java/Trinket/TrinketManagerTest.java
+++ src/test/java/Trinket/TrinketManagerTest.java
@@ -3,11 +3,16 @@ package Trinket;
 import me.plainoldmoose.trinket.Trinket;
 import me.plainoldmoose.trinket.TrinketManager;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockbukkit.mockbukkit.MockBukkit;
 import org.mockbukkit.mockbukkit.ServerMock;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,5 +43,14 @@ class TrinketManagerTest {
         tm.add(t);
         assertEquals(t, tm.get("trinket"));
         assertNotEquals(t1, tm.get("trinket"));
+    }
+
+    @Test
+    void managerNamespaceKeyTest() {
+        Trinket t = new Trinket("trinket", Material.DIAMOND);
+        tm.add(t);
+        Trinket t1 = tm.get("trinket");
+        PersistentDataContainer data = t1.getItem().getItemMeta().getPersistentDataContainer();
+        assertTrue(data.has(new NamespacedKey("moosestrinkets", "trinket")));
     }
 }
